b2a763454b15f35f10abc1454a4ac7e48c68abe4
Taki Vlasakakis <taki.vlasakakis@gmail.com>
2016-09-13 13:57:19 -0700

    finish all 5, starting 6.1 generating model, generated the comments model

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -11,6 +11,10 @@ class ArticlesController < ApplicationController
 		@article = Article.new
 	end
 
+	def edit
+		@article = Article.find(params[:id])
+	end
+
 	def create
 		@article = Article.new(article_params)
 
@@ -21,8 +25,25 @@ class ArticlesController < ApplicationController
 		end
 	end
 
-	private
-		def article_params
-			params.require(:article).permit(:title, :text)
+	def update
+		@article = Article.find(params[:id])
+
+		if @article.update(article_params)
+			redirect_to @article
+		else
+			render 'edit'
 		end
+	end
+
+	def destroy
+		@article = Article.find(params[:id])
+		@article.destroy
+		
+		redirect_to articles_path
+	end
+
+	private
+	def article_params
+		params.require(:article).permit(:title, :text)
+	end
 end
